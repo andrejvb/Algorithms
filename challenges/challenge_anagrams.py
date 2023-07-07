92db0bcee64412449adb4bf8c90b66f91918ae01
andrejvb <andre.jvb@gmail.com>
2023-07-07 20:28:37 -0300

    fifth requirement

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,25 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    string1 = first_string.lower()
+    string2 = second_string.lower()
+    sorted_string1 = quicksort(string1)
+    sorted_string2 = quicksort(string2)
+    if len(sorted_string1) | len(sorted_string2) == 0:
+        return (first_string, second_string, False)
+    string1_return = ''.join(sorted_string1)
+    string2_return = ''.join(sorted_string2)
+    return (string1_return, string2_return, string1_return == string2_return)
+        
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+        return quicksort(left) + [pivot] + quicksort(right)
+ 
+# arr = 'roma'
+# sorted_arr = quicksort(arr)
+print(is_anagram('amor', 'roma'))
+
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,5 +1,3 @@
 def is_palindrome_iterative(word):
     ...
-
-
-print(is_palindrome_iterative('agua'))
+    
