e1a58dfca7fe80e88c74a4ef71d58b6d017d2c72
andrejvb <andre.jvb@gmail.com>
2023-07-07 20:30:33 -0300

    fifth requirement lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -8,7 +8,7 @@ def is_anagram(first_string, second_string):
     string1_return = ''.join(sorted_string1)
     string2_return = ''.join(sorted_string2)
     return (string1_return, string2_return, string1_return == string2_return)
-        
+
 
 def quicksort(arr):
     if len(arr) <= 1:
@@ -18,8 +18,8 @@ def quicksort(arr):
         left = [x for x in arr[1:] if x < pivot]
         right = [x for x in arr[1:] if x >= pivot]
         return quicksort(left) + [pivot] + quicksort(right)
- 
+
+
 # arr = 'roma'
 # sorted_arr = quicksort(arr)
 print(is_anagram('amor', 'roma'))
-
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,2 @@
 def is_palindrome_iterative(word):
     ...
-    
