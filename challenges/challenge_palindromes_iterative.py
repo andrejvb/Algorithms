332c975289aeeb2f9e43258d4e2a3ae8737c5d8b
andrejvb <andre.jvb@gmail.com>
2023-07-07 19:16:28 -0300

    fourth requirement

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,12 +1,5 @@
 def is_palindrome_iterative(word):
-    if len(word) <= 1:
-        return True
-    elif word == '':
-        return False
-    elif word[0] != word[-1]:
-        return False
-    else:
-        return is_palindrome_iterative(word[1:-1])
+    ...
 
 
 print(is_palindrome_iterative('agua'))
